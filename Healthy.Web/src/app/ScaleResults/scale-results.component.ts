9ccaa8b361538bff3b3cf7ff983f65ec5b351329
Markus Sjöholm <markus@slingingcode.net>
2018-01-21 22:22:56 +0100

    Added the first Angular components

---
+++ Healthy.Web/src/app/ScaleResults/scale-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scale-results',
+  templateUrl: './scale-results.component.html',
+  styleUrls: ['./scale-results.component.scss']
+})
+export class ScaleResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Healthy.Web/src/app/app.module.ts
+++ Healthy.Web/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './Persons/person.component';
+import { ScaleResultsComponent } from './ScaleResults/scale-results.component';
+import { PageNotFoundComponent } from './Misc/page-not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    ScaleResultsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
